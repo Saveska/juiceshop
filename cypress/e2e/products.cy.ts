1d1cc0037fed49eeffeee2f55f206c1a50976171
Maria <saveska.maria@gmail.com>
2023-04-29 19:32:26 +0200

    Changes to original

--- cypress/e2e/products.cy.ts
+++ cypress/e2e/products.cy.ts
@@ -7,11 +7,11 @@ describe('template spec', () => {
 
   beforeEach(function () {
     login.navigateToLoginAndCloseDialog('http://localhost:3000/login#/login');
-    login.login('sanja.gorgievska@gmail', '#Newuser12#');
+    login.login('saveska.maria@gmail.com', '123456');
     login.verifySuccessfullLogin();
   })
 
-  it('User should be able to add item into basket', () => {
+  it('Add item into basket', () => {
     cy.get('.mat-search_icon-search').click();
     cy.get('#mat-input-0').type('apple juice');
     cy.get('#mat-input-0').type('{enter}');
@@ -19,8 +19,29 @@ describe('template spec', () => {
     cy.get('.fa-3x.warn-notification').should('have.text', '1');
   })
 
-  it('User should be able to add item into basket with POM', () => {
-    products.addItemToBasket('apple juice');
-    products.verifyItemAddedToBasket('1');
+  // it('User should be able to add item into basket with POM', () => {
+  //   products.addItemToBasket('apple juice');
+  //   products.verifyItemAddedToBasket('1');
+  // })
+
+  it('Delete item from basket', () => {
+    cy.get('[aria-label="Show the shopping cart"]').click();
+    cy.get('.fa-trash-alt').click();
+    cy.get('#price').should('have.text', 'Total Price: 0¤');
   })
+
+
+  it('Item not in stock', () => {
+    cy.get('.mat-search_icon-search').click();
+    cy.get('#mat-input-0').type('OWASP Juice Shop "King of the Hill" Facemask');
+    cy.get('#mat-input-0').type('{enter}');
+    cy.get('[aria-label="Add to Basket"]').click();
+    cy.get('.mat-simple-snack-bar-content').should('have.text', 'We are out of stock! Sorry for the inconvenience.');
+  })
+
+  it('Item not in stock with POM', () => {
+    products.addItemToBasket('OWASP Juice Shop "King of the Hill" Facemask');
+    products.verifyItemAddedToBasket('0');
+  })
+
 })
